885494ec20845f382bc023d46b6bc70e8d3cdd01
Chris Simbulan <simbulan.c@gmail.com>
2024-04-20 19:18:19 -0400

    added missing definition

--- backend/models/Game.js
+++ backend/models/Game.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+const Schema = mongoose.Schema;
+
 const GameSchema = new mongoose.Schema(
   {
     name: { type: String, required: [true, "Please provide a name"] },
