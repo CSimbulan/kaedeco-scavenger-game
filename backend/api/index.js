128dd71ce56499c97a7ba44ceefcb8360a97599b
Chris Simbulan <simbulan.c@gmail.com>
2024-04-19 22:15:33 -0400

    vercel test

--- backend/api/index.js
+++ backend/api/index.js
@@ -27,11 +27,7 @@ if (process.env.NODE_ENV === "production") {
     );
   });
 } else {
-  app.get("/", (req, res) => {
-    res.json({
-        message: "Server running!"
-    })
-  });
+  app.get("/", (req, res) => res.send("Express on Vercel"));
 }
 
 // --------------------------DEPLOYMENT------------------------------
@@ -39,7 +35,7 @@ if (process.env.NODE_ENV === "production") {
 // Error Handler Middleware (Should be at the end of all middlewares)
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
   console.log(`Server running on PORT ${PORT}`)
