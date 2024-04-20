b448d28fb8c6377496d38605acfc33ff829d91a6
Chris Simbulan <simbulan.c@gmail.com>
2024-04-20 11:06:33 -0400

    removed server split

--- backend/api/server.js
+++ backend/api/server.js
@@ -20,19 +20,11 @@ app.use("/api/game", require("../routes/game"));;
 
 // --------------------------DEPLOYMENT------------------------------
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./client/build")));
 
-  app.get("*", (req, res) => {
-    return res.sendFile(
-      path.resolve(__dirname, "client", "build", "index.html")
-    );
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running");
-  });
-}
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
 
 // --------------------------DEPLOYMENT------------------------------
 
