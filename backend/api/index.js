0aecb72532c3b7244260be40e5b2b31d65db2fc3
Chris Simbulan <simbulan.c@gmail.com>
2024-08-16 13:01:44 -0400

    test cors 12

--- backend/api/index.js
+++ backend/api/index.js
@@ -13,6 +13,28 @@ const mergedTypeDefs = require("../graphql/typeDefs/index");
 const mergedResolvers = require("../graphql/resolvers/index");
 const errorHandler = require("../middleware/error");
 
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
+
 async function initServer() {
   const app = express();
 
@@ -30,11 +52,14 @@ async function initServer() {
 
   await server.start();
 
+
+
   app.use(express.json());
   connectDB(); // Connect to databse
   app.use(
     "/",
     cors(),
+    allowCors(handler),
     expressMiddleware.expressMiddleware(server, {
       context: async ({ req }) => ({ token: req.headers.token }),
     })
