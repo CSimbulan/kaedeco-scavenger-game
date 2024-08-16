e12e31e0d7eaae841bce931306782d7f8f43e22b
Chris Simbulan <simbulan.c@gmail.com>
2024-08-16 03:54:51 -0400

    added apollo client

--- client/src/index.js
+++ client/src/index.js
@@ -2,17 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 
 import App from "./App";
 import "./App.css";
 import AuthProvider from "./context/AuthProvider";
+
+const client = new ApolloClient({
+  uri: `${process.env.REACT_APP_API_URL}`,
+  cache: new InMemoryCache(),
+  credentials: "include",
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <ApolloProvider client={client}>
+          <App />
+        </ApolloProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
